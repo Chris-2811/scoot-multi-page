8308aef1dea78c7996f2bf8ae724f1ad78e72200
Chris-2811 <christophschmid01@gmail.com>
2023-11-16 15:04:18 +0100

    add location to vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default {
         main: 'index.html',
         about: 'about.html',
         careers: 'careers.html',
+        locations: 'location.html',
         // Add more pages as needed
       },
     },
